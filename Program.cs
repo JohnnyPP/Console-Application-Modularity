4146928b31e9f688a0d08427c460367372c5c911
JohnnyPP <johnnnyppp@gmail.com>
2013-06-29 00:38:17 +0200

    Part 1 http://youtu.be/KTtTW-BmXJg?t=6m25s

--- Program.cs
+++ Program.cs
@@ -10,6 +10,44 @@ namespace Modularity
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (commands.Contains(input))
+                {
+                    int index = Array.IndexOf(commands, input);
+                    int x = int.Parse(Console.ReadLine());
+                    int y = int.Parse(Console.ReadLine());
+
+                    int result = funcs[index](x, y);
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Mistake!");
+                }
+            }
         }
+        static int Add(int x, int y)
+        {
+            return (x + y);
+        }
+        static int Sub(int x, int y)
+        {
+            return (x - y);
+        }
+        static int Mul(int x, int y)
+        {
+            return (x * y);
+        }
+        static int Div(int x, int y)
+        {
+            return (x / y);
+        }
+
+        static string[] commands = { "add", "sub", "mul", "div" };
+        static Func<int, int, int>[] funcs = { Add, Sub, Mul, Div };
+
     }
 }
